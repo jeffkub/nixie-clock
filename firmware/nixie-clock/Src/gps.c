a831d1e5fd20768305f1314a21d901d384a8c164
jeffkub <jeffkub@gmail.com>
2017-06-17 21:57:32 -0400

    Working on gps module

--- firmware/nixie-clock/Src/gps.c
+++ firmware/nixie-clock/Src/gps.c
@@ -1,5 +1,7 @@
 #include "gps.h"
 
+#include <stdbool.h>
+
 #include "FreeRTOS.h"
 #include "portmacro.h"
 #include "task.h"
@@ -8,12 +10,28 @@
 #include "stm32f3xx_hal.h"
 #include "stm32f3xx_hal_uart.h"
 
+#include "main.h"
+
 extern UART_HandleTypeDef huart3;
 
 static SemaphoreHandle_t devMutex;
 static SemaphoreHandle_t doneSem;
 
+static void gpsEnable(bool state);
+
 static ssize_t uart_read(char * data, size_t len);
+
+static void gpsEnable(bool state)
+{
+	if(state)
+	{
+		HAL_GPIO_WritePin(GPS_RESET_GPIO_Port, GPS_RESET_Pin, GPIO_PIN_SET);
+	}
+	else
+	{
+		HAL_GPIO_WritePin(GPS_RESET_GPIO_Port, GPS_RESET_Pin, GPIO_PIN_RESET);
+	}
+}
 
 static ssize_t uart_read(char * data, size_t len)
 {
@@ -44,6 +62,8 @@ void gps_init(void)
 {
     devMutex = xSemaphoreCreateMutex();
     doneSem = xSemaphoreCreateBinary();
+
+    gpsEnable(true);
 
 	return;
 }
