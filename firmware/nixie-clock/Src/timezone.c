eaa8ba4d27d78f7eccdbc4a0a6583af4a3fd5a43
Jeff Kubascik <jeffkub@gmail.com>
2017-07-30 12:28:40 -0400

    Working on timezone module

--- firmware/nixie-clock/Src/timezone.c
+++ firmware/nixie-clock/Src/timezone.c
@@ -1,28 +1,40 @@
 #include "timezone.h"
 
-//static struct timeZoneRule dst;
-//static struct timeZoneRule std;
+#define SECS_PER_HOUR (3600)
+#define SECS_PER_DAY  (SECS_PER_HOUR * 24)
+#define DAYS_PER_WEEK (7)
+
+//static timeZoneRule_t dst;
+//static timeZoneRule_t std;
 
 //static time_t dstUTC;
 //static time_t stdUTC;
 //static time_t dstLoc;
 //static time_t stdLoc;
 
-static int year(time_t time);
-static time_t toTime(struct timeZoneRule rule, int year);
+static time_t toTime_t(const timeZoneRule_t* rule, int year);
 
-static int year(time_t time)
+static time_t toTime_t(const timeZoneRule_t* rule, int year)
 {
     struct tm ts = {0};
 
-    localtime_r(&time, &ts);
+    /* Set time structure to the first day of the month of the rule.  For the
+     * last week rule, treat as the first week of the next month  */
+    ts.tm_hour = rule->hour;
+    ts.tm_mday = 1;
+    ts.tm_mon  = (rule->week == Last) ? (rule->month + 1) : (rule->month);
+    ts.tm_year = year;
 
-    return ts.tm_year + 1900;
-}
+    /* Adjust time structure and calculate day of week */
+    mktime(&ts);
 
-static time_t toTime(struct timeZoneRule rule, int year)
-{
-    return 0;
+    /* Adjust for for day of week */
+    ts.tm_mday += (rule->dow - ts.tm_wday + DAYS_PER_WEEK) % DAYS_PER_WEEK;
+
+    /* Adjust for week of month.  For last week rule, subtract one week. */
+    ts.tm_mday += (rule->week == Last) ? (-7) : (rule->week * DAYS_PER_WEEK);
+
+    return mktime(&ts);
 }
 
 time_t toLocal(time_t utc)
--- firmware/nixie-clock/Src/timezone.h
+++ firmware/nixie-clock/Src/timezone.h
@@ -3,19 +3,19 @@
 
 #include <time.h>
 
-enum week_t  {Last, First, Second, Third, Fourth}; 
-enum dow_t   {Sun=1, Mon, Tue, Wed, Thu, Fri, Sat};
-enum month_t {Jan=1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+enum week_t  { First=0, Second, Third, Fourth, Last }; 
+enum dow_t   { Sun=0, Mon, Tue, Wed, Thu, Fri, Sat };
+enum month_t { Jan=0, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
 
-struct timeZoneRule
+typedef struct timeZoneRule
 {
-    char    abbrev[6]; /* Five chars max */
-    uint8_t week;      /* First, Second, Third, Fourth, or Last week of the month */
-    uint8_t dow;       /* day of week, 1=Sun, 2=Mon, ... 7=Sat */
-    uint8_t month;     /* 1=Jan, 2=Feb, ... 12=Dec */
-    uint8_t hour;      /* 0-23 */
-    int     offset;    /* offset from UTC in minutes */
-};
+    char abbrev[6]; /* Five chars max */
+    int  week;      /* First, Second, Third, Fourth, or Last week of the month */
+    int  dow;       /* day of week, 0=Sun, 1=Mon, ... 6=Sat */
+    int  month;     /* 0=Jan, 1=Feb, ... 11=Dec */
+    int  hour;      /* 0-23 */
+    int  offset;    /* offset from UTC in minutes */
+} timeZoneRule_t;
 
 time_t toLocal(time_t utc);
 time_t toUTC(time_t local);
